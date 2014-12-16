ad3dd5ea4c6a1c63537a22e163f52990650a7a51
Tony Ruiz <tonytypes@gmail.com>
2014-12-15 20:55:22 -0500

    deleting old assessment 2 files

--- Lessons/Lesson04/Lesson04.playground/section-1.swift
+++ Lessons/Lesson04/Lesson04.playground/section-1.swift
@@ -1,15 +1,111 @@
 // Playground - noun: a place where people can play
 // Lesson 04
 
+//func helloWorld() {
+//    println("hello function")
+//    println("again!")
+//}
+
+//helloWorld()
+//helloWorld()
+//helloWorld()
+//helloWorld()
+//helloWorld()
+
+
+//var name = "Rudd"
+//func helloWorld(name: String) {
+//    println("Hello \(name)")
+//}
+//
+//helloWorld(name)
+
+//var name = "Rudd"
+//func helloWorld(name: String) {
+//    println("Hello \(name)!!!")
+//}
+//
+//helloWorld("Rudd")
+//helloWorld("Sean")
+//helloWorld("Travis")
+
+
+//If we want to print a line for two names, we can by doing this:
+
+func helloWorldToTwoPeople(nameOne: String, nameTwo: String) {
+    println("Hello \(nameOne) and \(nameTwo)!")
+}
+
+helloWorldToTwoPeople("Sean", "Travis")
+
+func formattedHelloWorldTextToName(name: String) -> String {
+    return "Hello \(name), have a great class!"
+}
+
+println(formattedHelloWorldTextToName("Tony") + " Woohoo!")
+var formattedText = formattedHelloWorldTextToName("Tony")
+println(formattedText)
+
+func lengthOfNames(nameOne: String, nameTwo: String) -> (Int, Int) {
+    // A function MUST return something, or else Swift doesn't know what to do with it
+    return (countElements(nameOne), countElements(nameTwo))
+}
+
+lengthOfNames("Tony","Lazaro")
+
+var lengths = lengthOfNames("Tony", "Lazaro")
+println(lengths.0)
+println(lengths.1)
+
 // Complete these, in order, writing code under each TODO statement. Each statement calls for a function to be written, write each of them and then immediately call it after the function definition.
 
 // TODO: Write a function that prints out "Hello world!" 10 times
 
+//var message = "Hello World"
+//func helloWorldTen() {
+//    var counter = 0
+//    while counter < 10 {
+//        counter = counter + 1
+//        println("Hello world")
+//    }
+//}
+//
+//helloWorldTen()
+
 // TODO: Write a function that prints out "Hit this line {number of iterations of the loop} times!" 20 times
+
+//func hitThisLine() {
+//    var counter = 0
+//    while counter < 20 {
+//        counter = counter + 1
+//        println("Hit this line \(counter) times!")
+//    }
+//}
+//
+//hitThisLine()
 
 // TODO: Write a function that accepts a string as a parameter. Print "Hello {value of string}!"
 
+//func stringAsParameter (name: String) {
+//    println("Hello \(name)")
+//}
+//
+//stringAsParameter("Andrew")
+//stringAsParameter("Tony")
+
+
 // TODO: Write a function accepts a string optional. If the string optional exists, print "Hello {value of string}!". If it doesn't, print "Hello world!"
+
+//func stringOptional (name: String?) {
+//    if let n = name {
+//        println("Hello \(n)")
+//    } else {
+//        println("Hello world!")
+//    }
+//}
+//
+//stringOptional("Tony")
+//stringOptional(nil)
 
 // TODO: Write a function that takes one parameter, n, and returns an integer, the nth series in the fibonacci
 // sequence. The first fibonacci number is 0, the second is 1, the third is 1, the fourth is 2, fifth is 3, sixth is 5, etc. fibonacci numbers at sequence n are the sum of the n-1 and n-2 fibonacci number.
