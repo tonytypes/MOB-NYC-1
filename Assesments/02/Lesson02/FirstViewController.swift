9443b1cdda30ae149873bb3a8a50b1a7c726f2a5
Tony Ruiz <tonytypes@gmail.com>
2014-12-17 20:05:44 -0500

    restore the corrupted lesson02 files to the very first time they were pushed to my repository

--- Assesments/02/Lesson02/FirstViewController.swift
+++ Assesments/02/Lesson02/FirstViewController.swift
@@ -16,5 +16,4 @@ class FirstViewController: UIViewController {
     TODO three: Hook up the button to a NEW function (in addition to the two above). Print “You can drink” below the above text if the user is above 21. If they are above 18, print “you can vote”. If they are above 16, print “You can drive”
     TODO four: Hook up the button to a NEW function (in additino to the three above). Print “you can drive” if the user is above 16 but below 18. It should print “You can drive and vote” if the user is above 18 but below 21. If the user is above 21, it should print “you can drive, vote and drink (but not at the same time!”.
     */
-    @IBOutlet weak var label: UILabel!
 }
